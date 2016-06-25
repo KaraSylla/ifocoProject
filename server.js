ec1834668989afe934cad470d8da5483d28e01d0
Kara Sylla <karalchimist@gmail.com>
2016-06-25 21:10:47 +0200

    add project ifocop (first commit)

---
+++ server.js
@@ -0,0 +1,168 @@
+// déclaration & lancement du module express
+var express = require('express');
+var app = express();
+
+// session
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+// charge module dotenv pour utiliser les variables d'environnement
+var dotenv = require('dotenv');
+// utilisations des utilitaires
+var utilitaires = require('./utilitaires');
+
+// récupère les valeurs du body
+var bodyParser = require('body-parser');
+// charge module mongodb + récupération de la propriété Mongoclient de l'obj mongodb
+var MongoClient = require('mongodb').MongoClient;
+
+// url de connexion à la base mongodb
+var URL ='mongodb://localhost:27017/socialNetwork';
+// déclaration variable pour la bdd
+var maDb;
+
+
+//chargement des variables d'environnement du fichier .env
+dotenv.load();
+// app.set('port', process.env.PORT || 25);
+console.log(dotenv.load())
+// fixation du moteur de visualisation & indication de l'emplacement des fichiers jade
+app.set('view engine', 'jade');
+app.set('views', 'jadefiles');
+
+// gestion des fichiers statiques
+app.use(express.static(__dirname + '/staticfiles/src'));
+app.use(express.static(__dirname + '/staticfiles/img'));
+
+// gestion des sessions
+// app.use(session({
+//   secret:'motdepassequiretouvelefichiercorrespondantauclienthttp01++',
+//   saveUninitialized : true,
+//   resave: false
+//   // maxAge: 900000000000000000000000000000000000000000000000
+// }));
+
+// app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+// app.post('/verification-formulaire-inscription', (req, res) => {
+//
+// });
+// gestion des routes en mode non connecté
+app.get('/', (req, res) => {
+  // initialisation des données de la session
+  // var collection = maDb.collection('');
+  // maDb.collection
+  res.render('login');
+});
+
+app.get('/inscription', (req, res) => {
+  // initialisation des données de la session
+
+  var collection = maDb.collection('members');
+  // collection.insert({pseudonyme :'ton père', mdp: 'tasoeur'}, function (err, result) {
+  //   console.log(result);
+  // });
+  res.render('inscription');
+});
+
+
+app.get('/index', (req, res) => {
+  // initialisation des données de la session
+  res.render('index');
+
+});
+
+app.post('/verification-formulaire-inscription', (req, res) => {
+  //
+  // utilitaires.emailConfirmation(req,res);
+  var messageErr = '';
+  var messageSucces = '';
+  if(req.body.pseudo == '' || req.body.firstMdp == '' || req.body.secondMdp == '') {
+    messageErr += 'veuillez saisir tous les champs';
+    res.render('login', {msgErr : messageErr});
+  }else {
+    if (req.body.firstMdp != req.body.secondMdp) {
+        messageErr += 'les mots de passe ne correspondent pas';
+        res.render('login', {msgErr : messageErr});
+    }else {
+      // gestion bdd
+      var collection = maDb.collection('membres');
+      collection.find({pseudo: req.body.pseudo}).toArray((err, data) => {
+        if (data == '') {
+          collection.insert({pseudo : req.body.pseudo, firstMdp: req.body.firstMdp, secondMdp:req.body.secondMdp}, (err, result) => {
+            collection.find({pseudo:req.body.pseudo}).toArray(function (err, data) {
+              messageSucces += 'votre compte est bien créé';
+              res.render('login', {msgeSucces : messageSucces});
+            });
+          });
+        }else {
+          // identifiant ou mot de passe existant dans la bdd
+          messageErr += 'identifiant déja existant';
+          res.render('login', {msgErr : messageErr});
+        }
+      });
+    }
+  }
+});
+
+app.post('/test', (req, res) => {
+  utilitaires.emailConfirmation(req,res);
+});
+
+
+
+
+
+
+// page des scores
+// app.get('/resultat', (req, res, next) => {
+//
+// });
+
+// gestion des routes pour les formulaires ...
+// app.get('/formulaire-inscription', (req, res) => {
+//   // initialisation des données de la session
+//
+// });
+
+// app.get('/formulaire-connexion', (req, res) => {
+//   // initialisation des données de la session
+//
+// });
+
+// gestion des routes en mode connecté
+// app.get('/jeumulti', (req, res, next) => {
+// });
+
+
+///////////////////////////////////////////////SOCKET///////////////////////////
+// association du serveur websocket au serveur http ce serveur
+// accept une requette upgrade (connexion persistante)
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+// gestion d'une requête WebSocket provenant d'un client WebSocket
+// socket est un objet représentant la connexion
+
+
+
+
+
+
+
+
+// connexion à la base de donnée
+MongoClient.connect(URL, (err, db) => {
+  if(err){
+    return;
+    console.log('impossible de se connecter à la base');
+  }
+  maDb = db;
+  app.listen(1986, () => {
+    console.log('connecté');
+  });
+});
---
+++ utilitaires.js
@@ -0,0 +1,53 @@
+//module pour gérer les msg d'erreurs sur les urls en fonction du mode connecté ou non
+// exports.emailConfirmation = (req, res, next) => {
+//   if (!req.session.pseudo) {
+//     res.status(403);
+//     next();
+//     return true;
+//   }
+// };
+//
+// exports.initSession = (req, res) => {
+//   req.session.destroy();
+// };
+
+exports.testounet = function () {
+  console.log('TESTOUNET')
+};
+
+// gestion mail de confirmation
+var nodemailer = require('nodemailer');
+
+// gestion mail de confirmation
+exports.emailConfirmation = (req, res) => {
+
+  var transporter = nodemailer.createTransport({
+    service : 'Mailgun',
+    auth : {
+      user : process.env.MAILGUN_USERNAME,
+      pass: process.env.MAILGUN_PASSWORD
+    }
+    // 'smtps://kara.sylla%40gmail.com:alchimiste1986.@smtp.gmail.com'
+  });
+
+  var emailOptions = {
+    from: 'darkgenust@gmail.com',
+    to: req.body.email,
+    subject: 'mon objet depuis node js',
+    text: 'message de test (ceci est un mail de confirmation); voici votre email : ' + req.body.email + ' et votre mdp ' + req.body.password
+    // html : 'message de test (ceci est un mail de confirmation); <br> voici votre email : ' + req.body.email + ' et votre mdp ' + req.body.password
+  };
+
+  transporter.sendMail(emailOptions,  (error, info) => {
+    if (error) {
+      console.log('ça marche pas poulet :' + error);
+      res.redirect('/');
+    }else {
+      console.log('dans ta maman :' + info.response);
+      res.redirect('/');
+    }
+  });
+
+  // transporter.close();
+
+};
